d32d459d7e6141a0e3f51510c5351f22f7a2a314
OksanaVorobiova <ov172608@gmail.com>
2022-12-24 13:07:21 +0200

    task-08

--- hw_6/src/js/task-08.js
+++ hw_6/src/js/task-08.js
@@ -1,1 +1,18 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+    event.preventDefault();
+
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+    
+if (email.value === "" || password.value === "" ) {
+    return alert('fill in all fields');
+} else {
+    console.log(`login: ${email.value}; password: ${password.value}`);
+    event.currentTarget.reset();
+    }
+}
