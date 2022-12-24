34094c5d5e5dd072ea2bd9cd422401fd322016f1
OksanaVorobiova <ov172608@gmail.com>
2022-12-24 14:23:23 +0200

    task-10

--- hw_6/src/js/task-10.js
+++ hw_6/src/js/task-10.js
@@ -1,3 +1,35 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+function createBoxes(amount) {
+  const divArray = [];
+
+  for (let i = 0; i < amount; i += 1) {
+    const divEl = document.createElement('div');
+    divEl.style.width = `${30 + i * 10}px`;
+    divEl.style.height = `${30 + i * 10}px`;
+    divEl.style.backgroundColor = getRandomHexColor();
+    divArray.push(divEl);
+  }
+  return divArray;
+}
+
+function destroyBoxes() {
+  boxesEl.innerHTML = "";
+}
+
+const inputEl = document.querySelector('input');
+const buttonCreate = document.querySelector('button[data-create]');
+const buttonDestroy = document.querySelector('button[data-destroy');
+const boxesEl = document.getElementById('boxes');
+
+//inputEl.addEventListener('input', )
+
+buttonCreate.addEventListener('click', () => {
+  let boxes = createBoxes(inputEl.value);
+  boxesEl.append(...boxes);
+});
+
+buttonDestroy.addEventListener('click', destroyBoxes);
